8145fff97d9983f05daf3d547b6c14cbbddbe40d
kastastin <kostya242421@gmail.com>
2024-03-24 08:45:45 +0200

    initial setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-blue-500">Homepage</h1>
+
+      <Button size="lg">Button</Button>
+    </>
+  );
+}
