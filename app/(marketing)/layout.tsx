7e1cf10927d9cb420e99ce36480210c706ef3224
kastastin <kostya242421@gmail.com>
2024-03-24 11:20:49 +0200

    added marketing skeleton

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,20 @@
+import { Header } from "./header";
+import { Footer } from "./footer";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function MarketingLayout({ children }: Props) {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <Header />
+      
+      <main className="flex flex-1 flex-col items-center justify-center">
+        {children}
+      </main>
+      
+      <Footer />
+    </div>
+  );
+}
---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function HomePage() {
+  return (
+    <>
+      <h1>Home page</h1>
+    </>
+  );
+}
