f464079da54ecb6adcd9f42ddf7a04db717ecb9a
kastastin <kostya242421@gmail.com>
2024-03-26 13:29:03 +0200

    added getUnits query

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
-import { getUserProgress } from "@/db/queries";
+import { getUserProgress, getUnits } from "@/db/queries";
 
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { Header } from "./header";
@@ -9,8 +9,12 @@ import { UserProgress } from "@/components/user-progress";
 
 export default async function LearnPage() {
   const userProgressPromise = getUserProgress();
+  const unitsPromise = getUnits();
 
-  const [userProgress] = await Promise.all([userProgressPromise]);
+  const [userProgress, units] = await Promise.all([
+    userProgressPromise,
+    unitsPromise,
+  ]);
 
   // Redirect to courses page if user has no active course
   if (!userProgress || !userProgress.activeCourse) {
@@ -21,6 +25,12 @@ export default async function LearnPage() {
     <div className="flex gap-[48px] px-6">
       <FeedWrapper>
         <Header title={userProgress.activeCourse.title} />
+
+        {units.map((unit) => (
+          <div key={unit.id} className="mb-10">
+            {JSON.stringify(unit)}
+          </div>
+        ))}
       </FeedWrapper>
 
       <StickyWrapper>
