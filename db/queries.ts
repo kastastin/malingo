5d7a2deb0707403b1305d95429fd996cd5d25a4f
kastastin <kostya242421@gmail.com>
2024-03-27 08:12:37 +0200

    (bug fix): early return in getUnits

--- db/queries.ts
+++ db/queries.ts
@@ -69,6 +69,11 @@ export const getUnits = cache(async () => {
 
   const normalizedData = data.map((unit) => {
     const lessonsWithCompletedStatus = unit.lessons.map((lesson) => {
+      // If there are no challenges, the lesson is not completed
+      if (lesson.challenges.length === 0) {
+        return { ...lesson, completed: false };
+      }
+      
       const allCompletedChallenges = lesson.challenges.every((challenge) => {
         return (
           challenge.challengeProgress &&
